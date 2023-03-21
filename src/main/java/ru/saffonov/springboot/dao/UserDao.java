c36c39b46ceb57958f47832c8b7947591dac0c49
AndrewSaffonov <asaffonov@mail.ru>
2023-03-21 11:17:14 +0400

    1.3

--- src/main/java/ru/saffonov/springboot/dao/UserDao.java
+++ src/main/java/ru/saffonov/springboot/dao/UserDao.java
@@ -3,6 +3,7 @@ package ru.saffonov.springboot.dao;
 import ru.saffonov.springboot.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserDao {
     void saveUser(User user);
@@ -14,4 +15,16 @@ public interface UserDao {
     List<User> getAllUser();
 
     void userEditor(User user, Long id);
+
+    Optional get(long id);
+
+/*    List getAllUser();
+
+    void saveUser(T t);
+
+    void updateUser(T t, String[] params);
+
+    void deleteUser(T t);
+
+ */
 }
--- src/main/java/ru/saffonov/springboot/dao/UserDaoImpl.java
+++ src/main/java/ru/saffonov/springboot/dao/UserDaoImpl.java
@@ -5,30 +5,57 @@ import jakarta.persistence.*;
 import ru.saffonov.springboot.model.User;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Consumer;
+
 @Repository
 public class UserDaoImpl implements UserDao {
+
     @PersistenceContext
-    EntityManager entityManager;
+    private EntityManager entityManager;
 
     @Override
-    public void saveUser(User user) {
-        entityManager.persist(user);
+    public Optional get(long id) {
+        return Optional.ofNullable(entityManager.find(User.class, id));
     }
 
     @Override
-    public void deleteUserById(Long id) {
-        entityManager.remove(entityManager.find(User.class, id));
-    }
+    public void saveUser(User user) {entityManager.persist(user); }
+//    public void saveUser(User user) {executeInsideTransaction(entityManager -> entityManager.persist(user));}
 
     @Override
-    public User getUserById(Long id) {
-        return entityManager.find(User.class, id);
-    }
+    public void deleteUserById(Long id) {entityManager.remove(entityManager.find(User.class, id)); }
+//    public void deleteUser(User user) {executeInsideTransaction(entityManager -> entityManager.remove(user));}
 
     @Override
-    public List<User> getAllUser() {
-        return entityManager.createQuery("FROM User").getResultList();
+    public User getUserById(Long id) {return entityManager.find(User.class, id);}
+/*    public void getUserById(User user, String[] params) {
+        user.setId(Objects.requireNonNull(params[0], "Name cannot be null"));
+        user.setEmail(Objects.requireNonNull(params[1], "Email cannot be null"));
+        executeInsideTransaction(entityManager -> entityManager.merge(user));
     }
+*/
+    @Override
+    public List<User> getAllUser() { return entityManager.createQuery("FROM User").getResultList(); }
+/*    public List getAllUser() {
+        Query query = entityManager.createQuery("SELECT e FROM User e");
+        return query.getResultList();
+    }
+
+    private void executeInsideTransaction(Consumer action) {
+        EntityTransaction tx = entityManager.getTransaction();
+        try {
+            tx.begin();
+            action.accept(entityManager);
+            tx.commit();
+        }
+        catch (RuntimeException e) {
+            tx.rollback();
+            throw e;
+        }
+    }
+*/
 
     @Override
     public void userEditor(User user, Long id) {
